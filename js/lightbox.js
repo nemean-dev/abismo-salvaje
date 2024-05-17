0807c0269bb7a7e80b2c155f260fc7afb2455023
Leon Ramos <nemean.dev@gmail.com>
2024-05-17 12:02:32 -0600

    Add comments for clarity and correct URIs

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,46 +1,53 @@
-const stillsDirectory = window.location.origin + "/images/stills/";
+const stillsDirectory = window.location.origin + "/assets/images/stills/";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const lightboxModal = document.getElementById("lightboxModal");
+  const lightboxModal = document.getElementById("lightbox-modal");
 
-  //final image path is stillsDirectory/icarus/A.jpeg for example
+  //final image path is stillsDirectory/still/image.jpeg for example
   const imagePaths = {
     icarus: ["A.jpeg", "D.jpeg", "I.jpeg"],
     mantidae: [
-      "MANTIDAE_-43.jpg",
-      "MANTIDAE_-47.jpg",
-      "MANTIDAE_-3.jpg",
-      "MANTIDAE_-31.jpg",
-      "MANTIDAE_-45.jpg",
-      "MANTIDAE_-11.jpg",
+      "mantidae43.jpg",
+      "mantidae47.jpg",
+      "mantidae3.jpg",
+      "mantidae31.jpg",
+      "mantidae45.jpg",
+      "mantidae11.jpg",
     ],
     "sacred-home": [],
     samai: [],
   };
 
   lightboxModal.addEventListener("show.bs.modal", (event) => {
+    // Obtain the selected short
     const button = event.relatedTarget;
     const short = button.getAttribute("data-short-id");
+    const stills = imagePaths[short] || [];
 
     const modalTitle = lightboxModal.querySelector(".modal-title");
     const modalBody = lightboxModal.querySelector(".modal-body");
 
+    // Capitalise the short name for the title.
     modalTitle.textContent = short.charAt(0).toUpperCase() + short.slice(1);
     modalBody.innerHTML = "";
 
-    const stills = imagePaths[short] || [];
+    // Create bootstrap container and row for the images
     const container = document.createElement("div");
     container.className = "container-fluid";
     const row = document.createElement("div");
     row.className = "row";
 
     stills.forEach((still) => {
+      // create an img and enclosing bs col for each still
       const col = document.createElement("div");
       col.className = "col-md-6 p-2";
       const img = document.createElement("img");
+
+      // set image source to corresponding URI
       img.src = stillsDirectory + short + "/" + still;
       img.alt = "Still from " + short;
       img.className = "img-fluid";
+
       col.appendChild(img);
       row.appendChild(col);
     });
